c4f812a1c1b8edd7c37ad4546e77b8626ff9d4be
luc1dLife <luc1d_life@protonmail.com>
2020-08-15 15:18:37 -0600

    First Variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,5 @@
-window.alert("This is an alert! JavaScript is running!");
+var name = window.prompt("What is your robot's name?");
+
+function fight() {
+    window.alert("Welcome, " + name + ". The Battle Begins. . .");
+  }
